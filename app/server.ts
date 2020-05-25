43807472cd6af98ce61fe48bb049956cc8de2125
WhyK <windchimeyk@gmail.com>
2020-05-26 01:39:16 +0900

    update: 自分で作成したモジュールを追加

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,29 @@
 import { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
+import { extractObjectValue } from 'https://github.com/windchime-yk/deno-util/raw/master/util.ts';
 import { APP_PORT } from './utils/config.ts';
 
+interface User {
+  _id: string,
+  name?: string,
+  description?: string
+}
+
+const userList: User[] = [
+  {
+    _id: '293kfo23dkw',
+    name: 'user name',
+    description: 'えいえいえー'
+  },
+  {
+    _id: 'ganfagaa3dw',
+    name: 'user name2',
+    description: 'おいおいおー'
+  }
+]
 
 const app = serve({ port: APP_PORT });
 console.log(`http://localhost:${APP_PORT}`);
+console.log(extractObjectValue<User>(userList, 'description'));
 
 for await (const req of app) {
   req.respond({ body: 'Hello, world!!\n'});
