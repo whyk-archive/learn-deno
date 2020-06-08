e7901f83d5259f274f15f4f3cd49c7c19a826de9
WhyK <windchimeyk@gmail.com>
2020-06-08 21:58:44 +0900

    update: HTTPモジュールをServestに変更

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,18 @@
-import { serve } from './utils/deps.ts'
+import { createApp } from './utils/deps.ts'
 import { APP_PORT } from './utils/config.ts';
 
-const app = serve({ port: APP_PORT });
-console.log(`http://localhost:${APP_PORT}`);
+const app = createApp();
 
-for await (const req of app) {
-  req.respond({ body: 'Hello, world!!\n'});
-}
+app.handle('/', async (req) => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      'content-type': 'text/plain',
+    }),
+    body: 'Hello, Deno!!'
+  });
+});
+
+app.listen({port: APP_PORT});
+
+console.log(`http://localhost:${APP_PORT}`);
