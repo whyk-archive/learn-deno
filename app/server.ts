919e2b4109a5c8df7d17756a68c3cc1a5f99228f
WhyK <windchimeyk@gmail.com>
2020-06-08 19:09:10 +0900

    update: 不要な関数を削除

--- app/server.ts
+++ app/server.ts
@@ -1,28 +1,8 @@
-import { serve, extractObjectValue } from './utils/deps.ts'
+import { serve } from './utils/deps.ts'
 import { APP_PORT } from './utils/config.ts';
-
-interface User {
-  _id: string,
-  name?: string,
-  description?: string
-}
-
-const userList: User[] = [
-  {
-    _id: '293kfo23dkw',
-    name: 'user name',
-    description: 'えいえいえー'
-  },
-  {
-    _id: 'ganfagaa3dw',
-    name: 'user name2',
-    description: 'おいおいおー'
-  }
-]
 
 const app = serve({ port: APP_PORT });
 console.log(`http://localhost:${APP_PORT}`);
-console.log(extractObjectValue<User>(userList, 'description'));
 
 for await (const req of app) {
   req.respond({ body: 'Hello, world!!\n'});
--- app/utils/deps.ts
+++ app/utils/deps.ts
@@ -1,2 +1,1 @@
 export { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
-export { extractObjectValue } from 'https://github.com/windchime-yk/deno-util/raw/master/mod.ts';
