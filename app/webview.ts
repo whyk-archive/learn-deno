92935c001169c3395dbf54662b4d6022488a8326
WhyK <windchimeyk@gmail.com>
2020-10-27 21:16:34 +0900

    feat: add webview sample

---
+++ app/webview.ts
@@ -0,0 +1,18 @@
+import { WebView } from "https://deno.land/x/webview/mod.ts"; // utils/deps.tsに集約するとエラーが出るため個別import
+
+const html = `
+  <html>
+  <body>
+    <h1>Hello Deno!!</h1>
+  </body>
+  </html>
+`;
+
+await new WebView({
+  title: "Local webview_deno example",
+  url: `data:text/html,${encodeURIComponent(html)}`,
+  height: 600,
+  resizable: true,
+  debug: true,
+  frameless: false,
+}).run();
