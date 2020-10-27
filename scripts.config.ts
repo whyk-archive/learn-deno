260465f7eff19e381954867bf5bf7d2d988a3ffb
WhyK <windchimeyk@gmail.com>
2020-10-27 22:28:02 +0900

    feat(scripts): add server command

--- scripts.config.ts
+++ scripts.config.ts
@@ -2,6 +2,11 @@ import { DenonConfig } from "https://deno.land/x/denon@2.4.4/mod.ts";
 
 const config: DenonConfig = {
   scripts: {
+    server: {
+      cmd: "deno run app/server.tsx",
+      allow: ["read", "env", "net"],
+      watch: true,
+    },
     webview: {
       cmd: "deno run app/webview.ts",
       desc: "Run the deno-webview test",
