7737f9c7a14f9d43e5357371cbccc7b18c7fa18e
WhyK <windchimeyk@gmail.com>
2020-10-27 21:16:59 +0900

    feat: add Denon config file

---
+++ scripts.config.ts
@@ -0,0 +1,15 @@
+import { DenonConfig } from "https://deno.land/x/denon@2.4.4/mod.ts";
+
+const config: DenonConfig = {
+  scripts: {
+    webview: {
+      cmd: "deno run app/webview.ts",
+      desc: "Run the deno-webview test",
+      allow: ["env", "read", "write", "net", "plugin"],
+      unstable: true,
+      watch: true,
+    },
+  },
+};
+
+export default config;
